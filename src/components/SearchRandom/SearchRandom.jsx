157853ebdebfdc6436b12e587447af3312657674
Gabriel Rouco <r.g.rouco1@gmail.com>
2022-12-08 23:30:10 -0300

    "re estilado"

--- src/components/SearchRandom/SearchRandom.jsx
+++ src/components/SearchRandom/SearchRandom.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
+import styles from '../SearchBar/SearchBar.module.css'
 
 export default function SearchRandom(props) {
     const {onSearchRandom} = props
     // eslint-disable-next-line
     const [character, setCharacter] = useState("")
   return (
-    <div>
+    <div className={styles.searchBar}>
       <button onClick={() =>onSearchRandom(character)}>Personaje Random</button> 
     </div>
   )
